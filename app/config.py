28c538e619c7be17c3735ab1b4707a1b7c2cc3da
Owiti-Charles <mikeycharlesm7@gmail.com>
2019-09-07 14:02:40 +0300

    Create articles class that would be responsible for defining new instances of an article

--- app/config.py
+++ app/config.py
@@ -4,4 +4,4 @@ load_dotenv()
 
 class Config:
     API_KEY=os.environ.get("API_KEY")
-    NEWS_API_BASE_URL= 'https://newsapi.org/v2/{}?apiKey={}'
+    NEWS_API_BASE_URL='https://newsapi.org/v2/{}?api_key={}'
--- app/models/news.py
+++ app/models/news.py
@@ -1,4 +1,19 @@
 
+class Articles:
+    """ 
+    Articles class to define Article Objects
+    
+    """
+    def __init__(self, source, author, title, description, url, image_url, publish_time):
+
+        self.source = source
+        self.author = author
+        self.title = title
+        self.description = description
+        self.url = url
+        self.image_url = image_url
+        self.publish_time = publish_time
+        
 def class Sources:
     '''
     Sources class that defines each source object
