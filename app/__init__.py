b1be9b2607a30a83f7ca6581ac54f1676fd86f27
Owiti-Charles <mikeycharlesm7@gmail.com>
2019-09-07 15:07:55 +0300

    Add newsrequests.py file and create get_sources method to make the first api request

--- app/__init__.py
+++ app/__init__.py
@@ -3,4 +3,5 @@ from .config import Config
 
 app = Flask(__name__,instance_relative_config = True)
 app.config.from_object(Config)
+
 from app import views
--- app/news_requests.py
+++ app/news_requests.py
@@ -8,8 +8,21 @@ api_key = app.config['API_KEY']
 
 url = app.config['NEWS_API_BASE_URL']
 
-def get_news(sources):
+def get_sources(category):
     """
     function that gets response from the api call
     """
-    
+    articles_url = url.format(category, api_key)
+
+    with urllib.request.urlopen(articles_url) as url:
+        sources_data = url.read()
+        response = json.loads(sources_data)
+
+        sources_outcome = None
+        if response['sources']:
+            sources_outcome_items = response['sources']
+            sources_outcome = process_results(sources_outcome_items)
+    return sources_outcome
+
+
+
