d640d42223513e566590877e56d30f08f950e0d2
Owiti-Charles <mikeycharlesm7@gmail.com>
2019-09-07 22:10:17 +0300

    Display more news-sources details ie news description

--- app/config.py
+++ app/config.py
@@ -4,4 +4,4 @@ load_dotenv()
 
 class Config:
     API_KEY=os.environ.get("API_KEY")
-    NEWS_API_BASE_URL='https://newsapi.org/v2/sources?&category={}&apiKey={}'
+    NEWS_API_BASE_URL='https://newsapi.org/v2/sources?country=us&category={}&apiKey={}'
