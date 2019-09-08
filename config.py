2673465cf48dc65c8e57b5f5740fb7c9b5c9c3a3
Owiti-Charles <mikeycharlesm7@gmail.com>
2019-09-08 17:58:36 +0300

    style the home page

--- config.py
+++ config.py
@@ -6,14 +6,10 @@ class Config:
     API_KEY=os.environ.get("API_KEY")
     NEWS_API_BASE_URL='https://newsapi.org/v2/sources?country=us&category={}&apiKey={}'
     NEWS_ARTICLES_APL_URL='https://newsapi.org/v2/everything?q={}&apiKey={}'
-class ProdConfig(Config):
-    pass
-
 
 class DevConfig(Config):
     DEBUG = True
 
 config_options = {
-'development':DevConfig,
-'production':ProdConfig
+'development':DevConfig
 }
--- start.sh
+++
@@ -1,2 +0,0 @@
-export API_KEY=19dc0a8d083043a396e135f9ffe0c69d 
-python3.6 manage.py server
