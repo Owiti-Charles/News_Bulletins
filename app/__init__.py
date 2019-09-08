bf902f4b6752ad51516d2f286f660c7db68acce5
Owiti-Charles <mikeycharlesm7@gmail.com>
2019-09-08 15:31:35 +0300

    Move tests to the tests folder

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,6 @@ from config import config_options
 
 def create_app(config_name):
     app = Flask(__name__,instance_relative_config = True)
-    # app.config.from_object(Config)
     app.config.from_object(config_options[config_name])
 
     from .main import main as main_blueprint
@@ -13,5 +12,3 @@ def create_app(config_name):
     configure_request(app)
 
     return app
-
-# from app import views
--- manage.py
+++ manage.py
@@ -6,5 +6,15 @@ app = create_app('development')
 manager = Manager(app)
 manager.add_command('server',Server)
 
+@manager.command
+def test():
+    """
+    Running all the unit tests
+
+    """
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
 if __name__ == "__main__":
     manager.run()
